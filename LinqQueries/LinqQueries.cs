2cf329401d6d6c0c03329ae45f351736719c318c
Michal Ludik <michal.ludik@solarwinds.com>
2017-02-27 07:09:32 +0100

    added linq project

---
+++ LinqQueries/LinqQueries.cs
@@ -0,0 +1,39 @@
+﻿/*
+Linq pracuje jednotlivo s kazdym elementom vstupu, nie s celou kolekciou
+
+Where => vyvolava filtrovanu verziu vstupu
+OrderBy => vyvolava zoradenu verziu vstupu
+Select => vyvolava sekvenciu kde je kazdy vstupny element transformovany alebo zobrazeny jeho lambda expression
+
+where (filter) -> order (zoradenie) -> select (zobrazenie)
+*/
+
+using System;
+using System.Linq;
+
+namespace LinqQueries
+{
+    class LinqQueries
+    {
+        static void Main()
+        {
+            string[] names = {"Libor", "Michal", "Juraj", "Fero"};
+            
+            //uloz vsetky mena ktore maju "a"
+            var filteredNames = names.Where(n=>n.Contains("a"));
+            //vypis vsetky vyfiltrovane mena
+            foreach (var filteredName in filteredNames)
+            {
+                Console.WriteLine(filteredName);
+            }
+
+            //vyber mena obsajujuce "a", zorad ich podla dlzky a preved na velke pismena
+            var filteredAndOrdered = names.Where(n => n.Contains("a")).OrderBy(n => n.Length).Select(n => n.ToUpper());
+            foreach (var name in filteredAndOrdered)
+            {
+                Console.WriteLine(name);
+            }
+            Console.Read();
+        }
+    }
+}
