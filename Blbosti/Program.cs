2060d177aa99346ddc3600af492a257c7f248036
Michal Ludik <michal.ludik@solarwinds.com>
2017-06-15 11:37:14 +0200

    aa

---
+++ Blbosti/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Blbosti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Directory.GetCurrentDirectory());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UpravaExistujucehoSuboru/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpravaExistujucehoSuboru
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string installserverPs1 = "installServer.ps1";
+            string text = File.ReadAllText(installserverPs1);
+            text = text.Replace("127.0.0.1", "abc");
+            File.WriteAllText(installserverPs1,text);
+        }
+    }
+}
