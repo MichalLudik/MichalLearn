29133ba134eec9fbe4c44c915771e281ab42d4fd
Michal Ludik <michal.ludik@solarwinds.com>
2017-07-03 08:49:47 +0200

    Async

---
+++ AsyncProgramming/Program.cs
@@ -0,0 +1,58 @@
+﻿/* https://msdn.microsoft.com/library/hh191443(vs.110).aspx
+ * 2 druhy awaitable - Task a Task<T> (Task.Yield / WinRT)
+ * 
+ */
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncProgramming
+{
+    class Program
+    {
+        static void Main()
+        {
+            var demo = new AsyncExamples();
+            demo.NiecoUrob();
+            demo.NapisDoCmd();
+
+            while (true)
+            {
+                Console.WriteLine("Robim nieco v MAIN threade...");
+                Thread.Sleep(500);
+            }
+            Console.ReadLine();
+        }
+        
+    }
+
+    public class AsyncExamples
+    {
+        public async Task NiecoUrob()
+        {
+            await Task.Run(DlhoBeziaciProces);
+            //v pripade ze odmazem Task.Run procesy pobezia seriovo po sebe -> DlhoBeziaciProces();
+        }
+
+        public async Task NapisDoCmd() {
+            var returnedString = await Task.Run(VratString);
+            Console.WriteLine(returnedString);
+        }
+
+        private static async Task<string> DlhoBeziaciProces()
+        {
+            int counter;
+            for (counter = 0;  counter< 50; counter++)
+            {
+                Console.WriteLine(counter);
+                Thread.Sleep(300);
+            }
+            return "Counter = " + counter;
+        }
+        private static async Task<string> VratString() {
+            Thread.Sleep(5000);
+            return "Hotovo";
+        }
+    }
+}
