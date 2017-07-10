9d2f79f6ff1dc22391a25d9ec270c81e61e5c7c7
Michal Ludik <michal.ludik@solarwinds.com>
2017-07-10 15:42:01 +0200

    First

---
+++ InversionOfControl/Program.cs
@@ -0,0 +1,43 @@
+﻿/* IoC - pattern na implementaciu Dependency Inversion
+ * Dependency Inversion - High-level moduly nezavisia na low-level moduloch. Oboje zavisia na abstrakciach
+ *                      - Abstrakcie nezavisia na detailoch ale opacne
+ *  
+ */
+
+ 
+// Trieda Pracovnik je zavisla na triede Lopata
+//class Pracovnik
+//{
+//    Lopata lopata = new Lopata();
+//}
+//class Lopata
+//{ 
+//}
+
+namespace InversionOfControl
+{
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+
+    public interface IPouziLopatu
+    {
+        void AkciaPriPouzitiLopaty();
+    }
+
+    class Pracovnik
+    {
+        IPouziLopatu pouziLopatu = null;
+
+        public void Kopaj()
+        {
+            if (pouziLopatu != null)
+            {
+                //namapovanie Interface na konkretnu triedu
+            }
+        }
+    }
+}
