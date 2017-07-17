3bb17a72f279d9cd95dc484e51496d4d9890253b
Michal Ludik <michal.ludik@solarwinds.com>
2017-07-17 15:40:01 +0200

    + ako funguje garbage collection

--- Dispose/Program.cs
+++ Dispose/Program.cs
@@ -3,6 +3,22 @@
  * ak objekt Auto pouziva objekt Motorka, pri zavolani Auto.Dispose() sa zavola automaticky aj Motorka.Dispose()
  * 
  * Dispose() by sa malo volat na 'unmanaged resources' -> praca so subormi, sietove spojenie, ...
+ * 
+ * Roots - udrzuje objekt pri zivote
+ *       - je jedno z nasledujucich:
+ *          - lokalna premenna alebo parameter aktualne beziacej metody (alebo navazujucej metody v call stacku)
+ *          - staticka premenna
+ *          - objekt uchovavajuci objekty pripravene na finalizaciu (?)
+ *          
+ * Ako funguje GC?
+ *  1. GC prejde rooty a oznaci vsetky objekty na ktore siahne ako 'dostupne'
+ *  2. Objekty, ktore toto oznacenie nedostali su okamzite posunute na garbage collection
+ *  3. Objekty, ktore nemaju finalizery su okamzite zahodene
+ *  4. Objekty, ktore maju finalizery su posunute do spesl fronty kde sa ich fin. vykonaju a su posunute k zahodeniu v dalsom kole GC
+ *  
+ *  Optimalizacne techniky:
+ *   - 'generational' - vyuziva vedomost, ze niektore objekty su dlho trvajuce a tak neni potreba ich zakazdym kontrolovat
+ *   - halda velkych objektov - objekty vacsie ako nejaky threshold (85000 bajtov)
  */
 
 using System;
@@ -39,6 +55,22 @@ namespace Dispose
         public void Dispose()
         {
             // ToDo
+        }
+    }
+
+    // Finalizer
+    class Test
+    {
+        /* 1. Garbage Collector identifikuje nepouzite objekty zrale pre zmazanie
+         * 2. Tie bez finalizera su zmazane hned, tie s finalizerom su posunute do specialnej fronty
+         * 3a. Hotovo (program bezi dalej)
+         * 3b. Paralelne sa nastartuje Finalizer Thread a zacne postupne spracovavat frontu a finalizery jej objektov
+         * 4. Finalizery sa dokoncia, objekty su oznacene ako hotove (ako bez finalizeru) a pri najblizsom GC su zmazane
+         * - musia byt rychle, nedat sa zablokovat, nereferencovat ine finelizery, nehadzat exception
+         */
+        ~Test()// finalizer, nesmie mat parametre a volat base triedu
+        {
+            // logika finalizeru
         }
     }
 }
