f6667c55b23a5669b4a36e3edbe8bb0af0563854
Michal <michal.ludik@gmail.com>
2017-07-26 20:44:25 +0200

    Genericky interface a trieda

--- Generika/Generika.cs
+++ Generika/Generika.cs
@@ -5,8 +5,11 @@
  *  - type safety -> pracuje sa vzdy nad jednym datovym typom, nemoze sa stat ze by sa pomiesali napr int a string
  *  - performance -> neprebieha boxing a unboxing
  *  - prepouzitie binarneho kodu -> jeden kod pre viacero datovych typov
+ *  
+ *  Constrainty - str 119
  */
 
+using System;
 using System.Collections;
 
 namespace Generika
@@ -30,6 +33,36 @@ namespace Generika
 
             Vymen(ref a,ref b);
             Vymen(ref x, ref y);
+
+            Zoznam<string> zoznam = new Zoznam<string>("Ahoj");
+            zoznam.Vypis();
+            Zoznam<double> zoznam2 = new Zoznam<double>(2.2);
+            zoznam2.Vypis();
+            Zoznam<Clovek> clovek = new Zoznam<Clovek>(new Clovek());
+            clovek.Vypis();
+            Console.Read();
+        }
+    }
+    public class Clovek { }
+
+    public interface IZoznam<T>
+    {
+        T MyProperty { get; set; }
+    }
+
+    public class Zoznam<T> : IZoznam<T>
+    {
+        private T properta;
+        T IZoznam<T>.MyProperty { get { return properta; } set { value = properta; } }
+
+        public Zoznam(T typ)
+        {
+            properta = typ;
+            //properta = default(T); -> priradi defaultnu hodnotu reference type / value type premennej
+        }
+
+        public void Vypis() {
+            Console.WriteLine($"{properta.GetType()}");
         }
     }
 }
