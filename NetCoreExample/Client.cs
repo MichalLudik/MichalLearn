15f44c704def9119b1f3dbcad49739edad1e604e
Michal Ludik <michal.ludik@solarwinds.com>
2017-10-31 10:36:08 +0100

    branch cleaning

---
+++ NetCoreExample/Client.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace NetCoreClient
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length!=2)
+            {
+                throw new Exception("You must enter IP address of server and communicating port...");
+            }
+
+            var serverIpAddress = IPAddress.Parse(args[0]);
+            var port = Int32.Parse(args[1]);
+
+            TcpClient client = new TcpClient();
+            client.Client.Connect(serverIpAddress,port);
+
+            ReadDataLoop(client);
+            
+        }
+
+        private static void ReadDataLoop(TcpClient client)
+        {
+            while (true)
+            {
+                if (!client.Connected)
+                {
+                    break;
+                }
+                var receivedData = ReadData(client);
+                Console.WriteLine(receivedData);
+            }
+        }
+
+        static string ReadData(TcpClient client)
+        {
+            NetworkStream stream = client.GetStream();
+            byte[] myReadBuffer = new byte[1024];
+            StringBuilder myCompleteMessage = new StringBuilder();
+
+            do
+            {
+                var numberOfBytesRead = stream.Read(myReadBuffer, 0, myReadBuffer.Length);
+
+                myCompleteMessage.AppendFormat("{0}", Encoding.ASCII.GetString(myReadBuffer, 0, numberOfBytesRead));
+            }
+            while (stream.DataAvailable);
+            
+            var retVal = myCompleteMessage.ToString();
+            return retVal;
+        }
+    }
+}
---
+++ NetCoreServer/Server.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace NetCoreServer
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                throw new Exception("You must enter IP and port which the communication will perform on...");
+            }
+
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(new IPEndPoint(IPAddress.Parse(args[0]), Int32.Parse(args[1])));
+            
+            socket.Listen(1);
+
+            var acceptedData = socket.Accept();
+            var data = new byte[acceptedData.SendBufferSize];
+            int j = acceptedData.Receive(data);
+            var adata = new byte[j];
+            for (int i = 0; i < j; i++)
+            {
+                adata[i] = data[i];
+            }
+
+            var dat = Encoding.ASCII.GetString(adata);
+            Console.WriteLine(dat);
+        }
+    }
+}
