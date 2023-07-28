1281bda64a509af339025277f587cc88d7302e45
Michal Ludik <michal.ludik@solarwinds.com>
2023-07-28 14:45:08 +0200

    Init

---
+++ Reflection/ReflectionSample/DemoClasses.cs
@@ -0,0 +1,66 @@
+﻿namespace ReflectionSample
+{
+    public interface ITalk
+    {
+        void Talk(string message);
+    }
+
+    public class EmployeeMakerAttribute : Attribute
+    {
+
+    }
+
+    [EmployeeMaker]
+    public class Employee : Person
+    {
+        public string Company { get; set; }
+    }
+
+    public class Alien : ITalk
+    {
+        public void Talk(string message)
+        {
+            Console.WriteLine($"Alien is talking...: {message}");
+        }
+    }
+
+    public class Person : ITalk
+    {
+        public string Name { get; set; }
+        public int age;
+        private string _aPrivateField = "init private field value";
+
+        public Person()
+        {
+            Console.WriteLine("A person is being created.");
+        }
+
+        public Person(string name)
+        {
+            Console.WriteLine($"A person with name {name} is being created using a private ctor");
+            Name = name;
+        }
+
+        public Person(string name, int age)
+        {
+            Console.WriteLine($"A person with name {name} and age {age} is being created using a private ctor");
+            Name = name;
+            this.age = age;
+        }
+
+        public void Talk(string message)
+        {
+            Console.WriteLine($"Talking...: {message}");
+        }
+
+        protected void Yell(string message)
+        {
+            Console.WriteLine($"Yelling... {message}");
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {age} {_aPrivateField}";
+        }
+    }
+}
---
+++ Reflection/ReflectionSample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+
+//https://app.pluralsight.com/course-player?clipId=efac8473-0d05-4cdc-b39e-0593f0f0cb2d
+
+Console.Title = "Learning Reflection";
+
+//string name = "Michal";
+////var stringType = name.GetType();
+//var stringType = typeof(string);
+//Console.WriteLine(stringType);
+
+var currentAssembly = Assembly.GetExecutingAssembly();
+var types = currentAssembly.GetTypes();
+foreach (var type in types)
+{
+    Console.WriteLine(type.Name);
+}
+
+var oneTypeFromCurrentAssembly = currentAssembly.GetType("ReflectionSample.Person");
+
+Console.ReadLine();
