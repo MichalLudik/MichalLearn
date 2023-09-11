c2bf010214680322a969cd5f6a9053a687261358
Michal Ludik <michal.ludik@solarwinds.com>
2023-09-11 10:56:10 +0200

    Static

---
+++ ExclusiveOpenFile/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ExclusiveOpenFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = args[0];
+            FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.None);
+            fs.ReadByte();
+            Console.WriteLine("File \"{0}\" locked", path);
+            Console.ReadKey();
+            fs.Close();
+        }
+    }
+}
---
+++ Project1/Program.cs
@@ -0,0 +1,17 @@
+﻿Auto auto = new Auto();
+//vyfuk = 1
+Auto.Vyfuk = 6;
+// vyfuk = 6
+
+Auto auto2 = new Auto();
+// vyfuk = 6
+Console.ReadLine();
+
+internal class Auto
+{
+    public int Kolesa;
+    public int Dvere = 4;
+    public static int Vyfuk = 1;
+}
+
+
